25b0262185cd1f47d1a9f9e16fbf8592ddbcade9
ouo2u <abod98451@gmail.com>
2024-01-25 11:27:38 +0100

    UWUWUWUWUWUWUU

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int resultat;
+if (!int.TryParse(Console.ReadLine(), out resultat))
+    Console.WriteLine("Skriv in en siffra");
+    
+    bool TryParse(string s, out int result)
+    { 
+        int result=5;
+        
+        if (TryParse)
+        {
+            Console.WriteLine("funkar?"+ result);
+            
+        }
+        else
+        {
+            Console.WriteLine("gg");
+
+        }
+    
+    }
+   
